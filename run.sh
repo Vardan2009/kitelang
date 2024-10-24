e562914e6b694878c8c0ba79e88542ee5a1ce3e1
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2024-10-24 11:32:06 +0400

    Added `run.sh`

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+nasm -felf64 -o kbuild/source.o kbuild/source.asm
+if [ $? -ne 0 ]; then
+    echo "Error: nasm failed."
+    exit 1
+fi
+
+ld -o kbuild/source kbuild/source.o stdlib/obj/*.o
+if [ $? -ne 0 ]; then
+    echo "Error: ld failed."
+    exit 1
+fi
+
+clear
+kbuild/source
+echo "\n\n\n[Program exited with exit code $?]"
